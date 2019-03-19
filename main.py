7b13ae2370559a39fcceb7942f04975185367841
Sergey Leleko <sergey@leleko.ru>
2019-03-19 22:26:05 +0500

    main.py changed

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return "Главная страница"
 if __name__ == "__main__":
     app.run(debug=True)

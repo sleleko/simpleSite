210a3ca6cc499e2b3fc5b72d03dd038f489aa37f
Sergey Leleko <sergey@leleko.ru>
2019-03-24 22:02:44 +0500

    update routes

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
-def home():
+def index():
     return "Главная страница"
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/about")
+def about():
+    return "Обо мне"
+

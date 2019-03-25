d90bb03ed4261ffd80d75d260d7e8ea8e6c5d545
Sergey Leleko <sergey@leleko.ru>
2019-03-26 01:09:56 +0500

    recreate templates about amd contacts, now width extends from index template. Create new route, template and script for LogIn service of site

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SECRET_KEY'] = 'mysupersecret'
 
 from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -9,3 +10,12 @@ def index():
 @app.route('/about')
 def about():
     return  render_template('about.html', title='Обо мне')
+
+@app.route('/contacts')
+def contactme():
+    return render_template('contacts.html', title='Связаться со мной')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Вход", form=form)

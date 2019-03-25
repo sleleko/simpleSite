e7edae39cd2b1b93bb72be363aeb79e95c0e2469
Sergey Leleko <sergey@leleko.ru>
2019-03-26 00:29:33 +0500

    add route for aboutme page

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,6 @@ from app import app
 def index():
     return render_template('index.html', title='Главная')
 
+@app.route('/about')
+def about():
+    return  render_template('about.html', title='Обо мне')

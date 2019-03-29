3fd80270dbd1ca347090fee73cdf47f7baa43a7d
Sergey Leleko <sergey@leleko.ru>
2019-03-30 01:03:33 +0500

    begin integrating database support

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'mysupersecret'
+db = SQLAlchemy(app)
+migrate = Migrate(app. db)
 
-from app import routes
+from app import routes, models
 
 
 

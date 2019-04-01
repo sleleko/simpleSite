1bac1d279630f051bbea872d6bb5bae1057f209f
Sergey Leleko <sergey@leleko.ru>
2019-04-01 09:07:12 +0500

    try to create Pages Migration

--- app/models.py
+++ app/models.py
@@ -8,3 +8,15 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class SiteContent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    title = db.Column(db.String(40))
+    description = db.Column(db.String(100))
+    introtext = db.Column(db.String(120))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    content = db.Column(db.String(250))
+
+    def __repr__(self):
+        return '<User {}, Date {}>'.format(self.username, self.timestamp)

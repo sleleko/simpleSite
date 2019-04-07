43a2efdee010bc2297ea4ac9117ddcc413531d88
Sergey Leleko <sergey@leleko.ru>
2019-04-07 10:36:37 +0500

    added LoginManager

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -9,6 +11,7 @@ app.config.from_object(Config)
 app.config['SECRET_KEY'] = 'mysupersecret'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
 

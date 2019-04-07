eaac59912a4ef78f05d5839720dec0b340337d00
Sergey Leleko <sergey@leleko.ru>
2019-04-07 11:19:11 +0500

    fixed error in routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, flash, redirect, url_for, app
+from flask import render_template, flash, redirect, url_for
+from app import app
 from app.forms import LoginForm
 from flask_login import current_user, login_user, logout_user
 from app.models import User

95319cb3df376047275e8893577a7c1938906ac5
Sergey Leleko <sergey@leleko.ru>
2019-04-07 10:20:34 +0500

    added hashing and check passwords

--- app/models.py
+++ app/models.py
@@ -1,5 +1,14 @@
 from datetime import datetime
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+
+
+def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
 
 
 class User(db.Model):

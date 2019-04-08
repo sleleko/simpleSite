3e001dc1c564ae0b902075bdc0c4eff08e7c28c3
Sergey Leleko <sergey@leleko.ru>
2019-04-08 22:08:12 +0500

    moved user functions

--- app/models.py
+++ app/models.py
@@ -8,13 +8,6 @@ from app import login
 def load_user(id):
     return User.query.get(int(id))
 
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password)
-
-
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
-
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,6 +18,11 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)

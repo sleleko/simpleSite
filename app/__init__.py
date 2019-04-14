ec3df3560dd68d13ebf248df1185ecf5a6d81262
Sergey Leleko <sergey@leleko.ru>
2019-04-15 00:46:04 +0500

    temp enable app.debug and set hardly secret_key for debug

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,16 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = '123321'#app.config('SECRET_KEY')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+app.debug = True
+
 
 if not app.debug:
-
+    
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:

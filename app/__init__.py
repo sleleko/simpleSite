ee4b86925be887724135380d441acaace79c14f3
Sergey Leleko <sergey@leleko.ru>
2019-04-14 23:50:16 +0500

    fix app name

--- app/__init__.py
+++ app/__init__.py
@@ -41,7 +41,7 @@ if not app.debug:
     app.logger.addHandler(file_handler)
 
     app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
+    app.logger.info('simpleSite startup')
 
 
 from app import routes, models, errors
